22800cfb8c5cc1063a4b81fa5a3ee9031e9e500f
Yat <looyatfung@gmail.com>
2023-02-25 17:01:53 +0000

    finish sorting algorithm tutorial

---
+++ test/index.md
@@ -0,0 +1,3 @@
+# Sorting algorithm with SFML
+
+Using code by: [https://dev.to/sansh/make-a-sorting-algorithm-visualizer-using-c-and-sfml-d61](https://dev.to/sansh/make-a-sorting-algorithm-visualizer-using-c-and-sfml-d61)
--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,70 @@
 #include <SFML/Graphics.hpp>
+#include <unistd.h>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+void insertionSort();
+void dispSort(int id);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+sf::RenderWindow window(sf::VideoMode(1300,600), "SFML works!");
+const int n = 100;
+float blockH[100];
+unsigned int microsecond = 100000;
+bool sorted = false;
+
+int main() {
+  for (int i=0; i < n; i++) {
+    blockH[i] = (rand()%500);
+  }
+
+  while (window.isOpen()) {
+
+    sf::Event event;
+      while (window.pollEvent(event)) {
+
+        switch (event.type) {
+          case sf::Event::EventType::Closed:
+          window.close();
+          break;
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+      }
+    if(!sorted) {
+      dispSort(0);
+      insertionSort();
     }
+  }
 
-   return 0;
+  return 0;
 
 }
+
+
+void insertionSort() {
+  usleep(microsecond*5);
+  int i, key, j;
+  for (i=0; i < n; i++) {
+    key = blockH[i];
+    j = i - 1;
+
+    while (j >= 0 && blockH[j] > key) {
+      blockH[j+1] = blockH[j];
+      j = j - 1;
+      dispSort(j);
+    }
+    blockH[j + 1] = key;
+  }
+
+  sorted = true;
+  dispSort(i);  
+
+}
+
+void dispSort(int id) {
+  window.clear();
+  for (int i=0; i < n; i++) {
+    sf::RectangleShape block(sf::Vector2f(10, blockH[i]));
+    block.setPosition(i*12, 600-blockH[i]);
+    block.setFillColor(sorted || i==id? sf::Color::Green : sf::Color::White); window.draw(block);
+  }
+  window.display();
+}
+
+
